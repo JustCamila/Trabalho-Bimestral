7a86b24d77f1625133840a1956a79e93ca344b87
RhauanSilva <engs-rhauansilva@camporeal.edu.br>
2026-09-13 14:45:02 -0300

    arrumei uns erro de rota

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PizzaController;
+use App\Http\Controllers\PedidoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('pedidos', PedidoController::class);
+    Route::resource('pizzas', PizzaController::class);
+    Route::resource('categorias', CategoriaController::class);
 });
 
 require __DIR__.'/auth.php';
